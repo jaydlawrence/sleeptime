251026ee5213cd8f62cce903f064cb5e7905f54a
Jayd Lawrence <jayd.lawrence@gmail.com>
2016-02-05 20:16:57 +0000

    select timer function, dynamically switch out value input field, send value to server

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,13 +1,30 @@
 /** In this file, we create a React component which incorporates components provided by material-ui */
 
 import React from 'react';
+import RadioButton from 'material-ui/lib/radio-button';
+import RadioButtonGroup from 'material-ui/lib/radio-button-group';
 import RaisedButton from 'material-ui/lib/raised-button';
+import TextField from 'material-ui/lib/text-field';
 import Dialog from 'material-ui/lib/dialog';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
 import Colors from 'material-ui/lib/styles/colors';
 import FlatButton from 'material-ui/lib/flat-button';
 import queryString from 'query-string';
+
+const styles = {
+  block: {
+    maxWidth: 250
+
+  },
+  blockOuter: {
+      display: "inline-block"
+  },
+  radioButton: {
+    marginBottom: 16,
+    fontSize: "large"
+  },
+};
 
 const containerStyle = {
   textAlign: 'center',
@@ -23,7 +40,8 @@ const Main = React.createClass({
   getInitialState() {
     return {
       muiTheme: ThemeManager.getMuiTheme(LightRawTheme),
-      open: false,
+      functionType: "setPeriod",
+
     };
   },
 
@@ -41,23 +59,17 @@ const Main = React.createClass({
     this.setState({muiTheme: newMuiTheme});
   },
 
-  _handleRequestClose() {
-    this.setState({
-      open: false,
-    });
-  },
-
-  _handleTouchTap() {
-    this.setState({
-      open: true,
-    });
-  },
-
   _handleSendRequestTouchTap(func) {
-    this.sendRequest(func);
+
+    this.sendRequest(func, this.refs.timeInput.getValue());
   },
 
-  sendRequest(remoteFunction) {
+  _functonSelectorChange(e) {
+      this.setState({functionType: e.target.value});
+  },
+
+  sendRequest(remoteFunction, value) {
+    remoteFunction = remoteFunction || this.state.functionType;
     window.fetch(
       'https://api.particle.io/v1/devices/' + this.props.config.device_id + '/' + remoteFunction,
       {
@@ -67,7 +79,7 @@ const Main = React.createClass({
         },
         body: queryString.stringify({
           access_token: this.props.config.access_token,
-          args: '1'
+          args: value
         })
       }
     ).then(function(res) {
@@ -86,27 +98,61 @@ const Main = React.createClass({
         onTouchTap={this._handleRequestClose}
       />
     );
+    let timeValueTextBox = null;
+    switch (this.state.functionType) {
+        case "setEnd":
+            timeValueTextBox = (<div style={styles.block}>
+                <TextField
+                  hintText="End Time"
+                  ref="timeInput"
+                />
+                <br/>
+            </div>);
+            break;
+        case "setPeriod":
+            timeValueTextBox = (<div style={styles.block}>
+                <TextField
+                  hintText="Time"
+                  ref="timeInput"
+                />
+                <br/>
+            </div>);
+            break;
+    }
 
     return (
       <div style={containerStyle}>
-        <Dialog
-          open={this.state.open}
-          title="Super Secret Password"
-          actions={standardActions}
-          onRequestClose={this._handleRequestClose}
-        >
-          1-2-3-4-5
-        </Dialog>
-
-        <h1>material-ui</h1>
-        <h2>example project</h2>
+        <h1>Sleeptime</h1>
+        <h2>IoT Grow Clock</h2>
 
         <div>{this.state.data}</div>
-
-        <RaisedButton label="Super Secret Password" primary={true} onTouchTap={this._handleTouchTap} />
-
-        <RaisedButton label="setTimer" primary={false} onTouchTap={this._handleSendRequestTouchTap.bind(this, "setTimer")} />
-        <RaisedButton label="reset" primary={false} onTouchTap={this._handleSendRequestTouchTap.bind(this, "reset")} />
+        <div style={styles.blockOuter}>
+            <div style={styles.block}>
+                <RadioButtonGroup
+                    name="func"
+                    defaultSelected="setPeriod"
+                    labelPosition="left"
+                    ref="functionSelector"
+                    onChange={this._functonSelectorChange}
+                >
+                  <RadioButton
+                    value="setPeriod"
+                    label="Period"
+                    style={styles.radioButton}
+                  />
+                  <RadioButton
+                    value="setEnd"
+                    label="End Time"
+                    style={styles.radioButton}
+                  />
+                </RadioButtonGroup>
+            </div>
+            {timeValueTextBox}
+            <div>
+                <RaisedButton label="Set Timer For 1 Minute" primary={true} onTouchTap={this._handleSendRequestTouchTap.bind(this, null)} />
+                <RaisedButton label="reset" primary={false} onTouchTap={this._handleSendRequestTouchTap.bind(this, "reset")} />
+            </div>
+        </div>
       </div>
     );
   },
